64575bae07d4b71fe5902e36cd29d7dc68189fc5
Stefan Pisic <15stefanp15@gmail.com>
2023-04-02 14:15:48 -0400

    added boss room

--- Assets/EnterDoor4.cs
+++ Assets/EnterDoor4.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EnterDoor3 : MonoBehaviour
+public class EnterDoor4 : MonoBehaviour
 {
 
     private void OnTriggerEnter2D(Collider2D other){
         Debug.Log("hello");
-        SceneManager.LoadScene("BossLevel");
+        SceneManager.LoadScene("MainMenu");
     }
 }
