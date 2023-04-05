f22f7fc496b6e6e54db2048d99ef8145d0711fc9
Stefan Pisic <15stefanp15@gmail.com>
2023-04-04 23:34:07 -0400

    added score when you open chest

--- Assets/ChestOpen.cs
+++ Assets/ChestOpen.cs
@@ -6,7 +6,7 @@ public class ChestOpen : MonoBehaviour
 {
     private Animator chestAnim;
     public Chest chest; // reference to the Chest script component
-
+    public float score = 50;
     void Start()
     {
         GameObject.Find ("Chest").transform.localScale = new Vector3(0, 0, 0);
@@ -19,6 +19,7 @@ public class ChestOpen : MonoBehaviour
     void Update()
     {
         if (GameObject.Find("ChaseMob (1)") == null && GameObject.Find("ChaseMob (2)") == null && GameObject.Find("ChaseMob") == null){
+            Scoring.totalScore += score;
             GameObject.Find ("Chest").transform.localScale = new Vector3(0.3f, 0.28f, 1f);
             chestAnim = GetComponent<Animator>();
             Debug.Log("Starting state:" + chestAnim.GetBool("ChestOpen"));
